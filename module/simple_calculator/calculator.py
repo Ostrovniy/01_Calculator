b1cda7e17dbbb9aa846e428a397cead4d8709e21
Ostrovniy <ostrovniy0001@gmail.com>
2024-10-21 00:38:07 +0300

    creating calculation logic for a calculator
    1. Кнопка шаг назад
    2. Кнопка очистить поле ввода
    3. Кнопка шаг назад
    4. Кнопка равно

--- module/simple_calculator/calculator.py
+++ module/simple_calculator/calculator.py
@@ -20,7 +20,7 @@ class CalculatorFrame(tk.Frame):
         self.entry.grid(row=0, column=0, columnspan=5, sticky='wnes', pady=5)
 
         # Кнопка стереть последний введенный символ
-        self.back = ttk.Button(self, text='<', style='my.TButton')
+        self.back = ttk.Button(self, text='<', style='my.TButton', command=self.step_back)
         self.back.grid(row=1, column=0, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка расчитать процент
@@ -28,71 +28,101 @@ class CalculatorFrame(tk.Frame):
         self.percent.grid(row=1, column=1, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка очистить поле ввода
-        self.reset = ttk.Button(self, text='c', style='my.TButton')
+        self.reset = ttk.Button(self, text='c', style='my.TButton', command=self.clear_input_field)
         self.reset.grid(row=1, column=2, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка сложыть "+"
-        self.addition = ttk.Button(self, text='+', style='my.TButton')
+        self.addition = ttk.Button(self, text='+', style='my.TButton', command= lambda: self.add_symbol('+'))
         self.addition.grid(row=1, column=3, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 7
-        self.seven = ttk.Button(self, text='7', style='my.TButton')
+        self.seven = ttk.Button(self, text='7', style='my.TButton', command= lambda: self.add_symbol('7'))
         self.seven.grid(row=2, column=0, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 8
-        self.eight = ttk.Button(self, text='8', style='my.TButton')
+        self.eight = ttk.Button(self, text='8', style='my.TButton', command= lambda: self.add_symbol('8'))
         self.eight.grid(row=2, column=1, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 9
-        self.nine = ttk.Button(self, text='9', style='my.TButton')
+        self.nine = ttk.Button(self, text='9', style='my.TButton', command= lambda: self.add_symbol('9'))
         self.nine.grid(row=2, column=2, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка минус "-"
-        self.minus = ttk.Button(self, text='-', style='my.TButton')
+        self.minus = ttk.Button(self, text='-', style='my.TButton', command= lambda: self.add_symbol('-'))
         self.minus.grid(row=2, column=3, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 4
-        self.four = ttk.Button(self, text='4', style='my.TButton')
+        self.four = ttk.Button(self, text='4', style='my.TButton', command= lambda: self.add_symbol('4'))
         self.four.grid(row=3, column=0, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 5
-        self.five = ttk.Button(self, text='5', style='my.TButton')
+        self.five = ttk.Button(self, text='5', style='my.TButton', command= lambda: self.add_symbol('5'))
         self.five.grid(row=3, column=1, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 6
-        self.six = ttk.Button(self, text='6', style='my.TButton')
+        self.six = ttk.Button(self, text='6', style='my.TButton', command= lambda: self.add_symbol('6'))
         self.six.grid(row=3, column=2, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка деление "/"
-        self.division = ttk.Button(self, text='/', style='my.TButton')
+        self.division = ttk.Button(self, text='/', style='my.TButton', command= lambda: self.add_symbol('/'))
         self.division.grid(row=3, column=3, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 1
-        self.one = ttk.Button(self, text='1', style='my.TButton')
+        self.one = ttk.Button(self, text='1', style='my.TButton', command= lambda: self.add_symbol('1'))
         self.one.grid(row=4, column=0, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 2
-        self.two = ttk.Button(self, text='2', style='my.TButton')
+        self.two = ttk.Button(self, text='2', style='my.TButton', command= lambda: self.add_symbol('2'))
         self.two.grid(row=4, column=1, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 3
-        self.three = ttk.Button(self, text='3', style='my.TButton')
+        self.three = ttk.Button(self, text='3', style='my.TButton', command= lambda: self.add_symbol('3'))
         self.three.grid(row=4, column=2, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка умножение "*"
-        self.multiplication = ttk.Button(self, text='*', style='my.TButton')
+        self.multiplication = ttk.Button(self, text='*', style='my.TButton', command= lambda: self.add_symbol('*'))
         self.multiplication.grid(row=4, column=3, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка 0
-        self.zero = ttk.Button(self, text='0', style='my.TButton')
+        self.zero = ttk.Button(self, text='0', style='my.TButton', command= lambda: self.add_symbol('0'))
         self.zero.grid(row=5, column=0, columnspan=2, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка точка "."
-        self.point = ttk.Button(self, text='.', style='my.TButton')
+        self.point = ttk.Button(self, text='.', style='my.TButton', command= lambda: self.add_symbol('.'))
         self.point.grid(row=5, column=2, sticky='wnes', ipadx=20, ipady=20)
 
         # Кнопка равно "="
-        self.equals = ttk.Button(self, text='=', style='my.TButton')
+        self.equals = ttk.Button(self, text='=', style='my.TButton', command=self.ravno)
         self.equals.grid(row=5, column=3, sticky='wnes', ipadx=20, ipady=20)
+
+    
+    def add_symbol(self, symbol):
+        """Добавить символ в выражение"""
+        current_value = self.entry_str.get()
+        self.entry_str.set(current_value+symbol)
+
+    def clear_input_field(self):
+        """Очистить поле ввода"""
+        self.entry_str.set('')
+
+    def step_back(self):
+        """Удалить последний символ"""
+        current_value = self.entry_str.get() 
+        if len(current_value) > 0: 
+            self.entry_str.set(current_value[:-1])
+
+    def ravno(self):
+        """Обработка нажатия кнопки ="""
+        # Текущее выражение
+        current_value = self.entry_str.get()
+        # Очистить поле ввода
+        self.clear_input_field()
+        try:
+            # Расчитать математическое выражение
+            res = eval(current_value)
+            # записать результат в поле ввода
+            self.entry_str.set(res) 
+        except:
+            self.entry_str.set('Ошибка') 
 
         
